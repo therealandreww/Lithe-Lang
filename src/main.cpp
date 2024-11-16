a8da202b9a83852f68f62003b5cd1030b86ba11d
heewoonkim2020 <heewoonkim2020@gmail.com>
2024-11-16 22:34:37 +0900

    Modify msg

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@ using std::cin;
 using std::string;
 
 int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World!\n";
+    if (sizeof(argv) < 2) {
+        cout << "The Lithe Project 2024\n";
+        cout << "Check the online documentation: https://andrewslair.org\n";
+    }
     return 0;
 }
