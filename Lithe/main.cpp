c84706e1f31e443b17cf8c4640cf52956cec2524
heewoonkim2020 <85213715+heewoonkim2020@users.noreply.github.com>
2024-11-16 21:35:50 +0900

    Base init

--- Lithe/main.cpp
+++ Lithe/main.cpp
@@ -1,14 +1,6 @@
-//
-//  main.cpp
-//  Lithe
-//
-//  Created by 김희운 on 11/16/24.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
